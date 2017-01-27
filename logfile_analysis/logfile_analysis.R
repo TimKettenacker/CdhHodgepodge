16d470816b34e9e8e33bed8b30c11135e0c8d176
Tim <Tim.Kettenacker@gmail.com>
2017-01-27 17:47:30 +0100

    added logfile analyzer and logfile dummy

---
+++ logfile_analysis/logfile_analysis.R
@@ -0,0 +1,62 @@
+# aiming at analysis of logfiles for cdh
+# the logfile concatenates all relevant information in single lines per action
+# this is not very handy for analysis and visualization
+
+# to-do: some of it should be handled more gracefully, i.e. missing values
+#        some of the tasks can be capsuled in the toTime function
+
+require(ggplot2)
+require(plotly)
+
+# read in lines delimited by "/n"
+raw <- read.csv2("web-2017-01-25.log", sep = "\n", header = FALSE)
+
+# do a first cleanup
+cleansed_lines <- vector(mode = "list", length = nrow(raw))
+for(line in raw){
+  cleansed_lines <- gsub("\\[#1] |\\[01FF] INFO     Record saved: record\\(source=", replacement = "", line)
+}
+
+# extract date + time combination and grep keys
+datetime <- list()
+keys <- vector(mode = "list", length = nrow(raw))
+for(i in 1:length(cleansed_lines)){
+  key <- sub(".*?key=(.*?) .*", "\\1", cleansed_lines[i])
+  keys[[i]] <- key
+  cleansed_line_stripped <- unlist(strsplit(cleansed_lines[i], " ", fixed = TRUE))
+  datetime <- append(datetime, paste(cleansed_line_stripped[1], cleansed_line_stripped[2], sep = " "))
+}
+
+# function to display CET and fractional seconds
+toTime <- function(val){
+  op <- options(digits.secs = 3)
+  op
+  line <- sub(",", ".", val, fixed = TRUE)
+  time <- strptime(line, "%Y-%m-%d %H:%M:%OS")
+  return(time)
+}
+
+# iterate over toTime function to calculate time intervals
+store_time <- vector(mode = "list", length = length(datetime))
+for(i in 1:length(datetime)){
+  store_time[[i]] <- toTime(datetime[i])
+}
+
+time_interval <- list()
+for(n in 1:length(store_time)){
+  z <- store_time[[n+1]] - store_time[[n]]
+  time_interval <- append(time_interval, as.numeric(z))
+}
+
+# store all results in data frame
+keys[[i]] <- NULL
+df_timelapse <- data.frame(observation_point = 1:length(time_interval), keys = unlist(keys), time_interval = unlist(time_interval))
+
+# visualize elapsed time
+a <- ggplot(df_timelapse, aes(observation_point, time_interval))
+ggplotly(a + geom_line() + theme_bw() + xlab("number of processed records") + ylab("elapsed time between records in sec"))
+
+# write results to csv
+out_file <- paste(getwd(), "\\output.csv", sep="")
+write.csv2(df_timelapse, out_file, row.names = FALSE, quote = FALSE)
+
